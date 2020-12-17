8937857f6a8c08140982bc5765117e5ab43000a1
Dakuangming Zheng <729785176@qq.com>
2020-12-17 18:52:45 +0800

    add CommonResult.java and Payment pojo on dev Branch

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Payment.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+/**
+ * @author ：DaDa
+ * @description：
+ * @date ：2020/12/17 11:15
+ */
+public class Payment {
+    /*
+     * @author: DaDa
+     * @description: TODO
+     * @date: 2020/12/17 11:29
+     * @param str
+     * @return java.lang.String
+     */
+    public String test(String str){ return str; } 
+}
